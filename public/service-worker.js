45fc6ba946c12478be1a77bd963056acf23a00be
Adam <adam.kowalczuk@poczta.fm>
2020-12-28 16:27:25 +0100

    PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,23 @@
-console.log('Hello from service-worker.js');
 
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+    "/precache-manifest.6c05415315464b46ff516bca67982f54.js"
+);
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
+
+workbox.core.clientsClaim();
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+
+    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+});
