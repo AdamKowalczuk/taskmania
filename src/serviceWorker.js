dffafa38c69e8f3f68054aa2eb795c0024277320
Adam <adam.kowalczuk@poczta.fm>
2020-12-28 17:02:38 +0100

    install

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -23,48 +23,48 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/i
 
 //Install
 
-// let deferredPrompt;
+let deferredPrompt;
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//     // Prevent the mini-infobar from appearing on mobile
-//     e.preventDefault();
-//     // Stash the event so it can be triggered later.
-//     deferredPrompt = e;
-//     // Update UI notify the user they can install the PWA
-//     showInstallPromotion();
-// });
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+});
 
-// buttonInstall.addEventListener('click', (e) => {
-//     // Hide the app provided install promotion
-//     hideMyInstallPromotion();
-//     // Show the install prompt
-//     deferredPrompt.prompt();
-//     // Wait for the user to respond to the prompt
-//     deferredPrompt.userChoice.then((choiceResult) => {
-//         if (choiceResult.outcome === 'accepted') {
-//             console.log('User accepted the install prompt');
-//         } else {
-//             console.log('User dismissed the install prompt');
-//         }
-//     });
-// });
+buttonInstall.addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    hideMyInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+    });
+});
 
-// window.addEventListener('appinstalled', (evt) => {
-//     // Log install to analytics
-//     console.log('INSTALL: Success');
-// });
+window.addEventListener('appinstalled', (evt) => {
+    // Log install to analytics
+    console.log('INSTALL: Success');
+});
 
-// window.addEventListener('DOMContentLoaded', () => {
-//     let displayMode = 'browser tab';
-//     if (navigator.standalone) {
-//         displayMode = 'standalone-ios';
-//     }
-//     if (window.matchMedia('(display-mode: standalone)').matches) {
-//         displayMode = 'standalone';
-//     }
-//     // Log launch display mode to analytics
-//     console.log('DISPLAY_MODE_LAUNCH:', displayMode);
-// });
+window.addEventListener('DOMContentLoaded', () => {
+    let displayMode = 'browser tab';
+    if (navigator.standalone) {
+        displayMode = 'standalone-ios';
+    }
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        displayMode = 'standalone';
+    }
+    // Log launch display mode to analytics
+    console.log('DISPLAY_MODE_LAUNCH:', displayMode);
+});
 
 
 window.addEventListener('beforeinstallprompt', (event) => {
